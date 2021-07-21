de498c7b92c067aa019bd3086cfd14d1c2915b55
ethanlee <seungwoonlee90@gmail.com>
2021-07-21 15:09:10 +0900

    sign up

--- src/routes/SignIn.js
+++ src/routes/SignIn.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import firebase from "firebase/app";
 import "firebase/auth";
+import { db } from "../components/firebase";
 
 class SignIn extends React.Component {
 
@@ -21,16 +22,33 @@ class SignIn extends React.Component {
             window.location.href = '/'
         };
 
+        const Signup = async (e) => {
+            e.preventDefault();
+            await firebase.auth().createUserWithEmailAndPassword(document.getElementById('signup_email').value, document.getElementById('signup_password').value).then((result) => {
+
+                db.collection('user').doc(result.user.uid).set({
+                    email : document.getElementById('signup_email').value,
+                })
+                window.location.href = '/'
+            }
+            )};
+
         return (
             <div className="container">
                 <span>Sign In</span>
-                <input type="text" id="email" placeholder="email" />
-                <input type="password" id="password" placeholder="password" />
                 <form onSubmit = {Login}>
+                    <input type="text" id="email" placeholder="email" required />
+                    <input type="password" id="password" placeholder="password" required />
                     <button className="btn btn-danger mt-3" id="login">LogIn</button>
                 </form>
                 <form onSubmit = {Logout}>
                     <button className="btn btn-danger mt-3" id="logout">Logout</button>
+                </form>
+                <form onSubmit = {Signup}>
+                    <span>Sign Up</span>
+                    <input type="text" id="signup_email" placeholder="email" required />
+                    <input type="password" id="signup_password" placeholder="password" required />
+                    <button className="btn btn-danger mt-3" id="signup">LogIn</button>
                 </form>
             </div>
         )
