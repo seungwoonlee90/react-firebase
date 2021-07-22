a80060232f7d944191245b0485011b88b342659e
ethanlee <seungwoonlee90@gmail.com>
2021-07-23 00:59:08 +0900

    upload components

---
+++ src/components/Upload.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import firebase from 'firebase/app';
+import { db } from './firebase'
+import 'firebase/auth'
+
+class Upload extends React.Component {
+
+    render() {
+
+        const upload = async (e) => {
+            e.preventDefault();
+            await db.collection('product').add({
+              uid : firebase.auth().currentUser.uid,
+              title : document.getElementById('title').value,
+              body : document.getElementById('body').value,
+              date : new Date()
+        })
+        };
+
+        return(
+            <div className="container">
+                <span>Upload to firebase</span>
+                <form onSubmit= {upload}>
+                    <input type="text" id="title" placeholder="title" />
+                    <input type="text" id="body" placeholder="body" />
+                    <button className="btn btn-danger mt-3" id="send">올리기</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Upload;
--- src/routes/Upload.js
+++ src/routes/Upload.js
@@ -1,34 +1,13 @@
 import React from 'react';
-import firebase from "firebase/app";
-import 'firebase/storage';
-import 'firebase/auth'
+import Upload from '../components/Upload';
 
-class Upload extends React.Component {
+class UpLoad extends React.Component {
 
     render() {
-        const onSubmit = async (e) => {
-            e.preventDefault();
-            
-            const db = firebase.firestore();
-            await db.collection('product').add({
-              uid : firebase.auth().currentUser.uid,
-              title : document.getElementById('title').value,
-              body : document.getElementById('body').value,
-              date : new Date()
-        })
-        };
-
         return (
-            <div className="container">
-                <span>Upload to firebase</span>
-                <form onSubmit= {onSubmit}>
-                    <input type="text" id="title" placeholder="title" />
-                    <input type="text" id="body" placeholder="body" />
-                    <button className="btn btn-danger mt-3" id="send">올리기</button>
-                </form>
-            </div>
+            <Upload />
         )
     }
 }
 
-export default Upload
+export default UpLoad;
