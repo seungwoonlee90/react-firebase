704e8ed7127ff4494c182523384cda974909ed8d
ethanlee <seungwoonlee90@gmail.com>
2021-08-18 14:58:24 +0900

    App.js create footer

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -3,7 +3,12 @@ import AppRouter from './Router';
 
 function App() {
   const [isLoggedIn] = useState(true);
-  return <AppRouter isLoggedIn={isLoggedIn} />
+  return(
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  )
 }
 
 export default App;
