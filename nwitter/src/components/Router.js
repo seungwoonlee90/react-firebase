94ee2688ca9db64c64287398882413bf4eaf30f3
ethanlee <seungwoonlee90@gmail.com>
2021-08-18 10:56:15 +0900

    Router.js fixed

--- nwitter/src/components/Router.js
+++ nwitter/src/components/Router.js
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
 import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
 
 
-export default () => {
-    const [isLoggedIn, setIsLoggedIn] = useState();
+const AppRouter =  () => {
+    const [isLoggedIn] = useState(true);
     return (
         <Router>
             <Switch>
-                {isLoggedIn? show Home : show login page}
+                {isLoggedIn? (
+                <> 
+                <Route exact path ="/">
+                    <Home />
+                </Route>
+                </>)
+                : (
+                <Route exact path ="/">
+                    <Auth />
+                </Route>)
+                }
             </Switch>
         </Router>
     )
 }
+
+export default AppRouter;
