793dc8559f2f4def2b1485b837a7161b34b8b783
ethanlee <seungwoonlee90@gmail.com>
2021-08-23 21:37:16 +0900

    Log In Form

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,4 +1,13 @@
 import React from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => <div>
+    <form>
+        <input type="text" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <input type="submit" value="LogIn"/>
+    </form>
+    <div>
+        <button>Continue with google</button>
+    </div>
+</div>;
 export default Auth;
