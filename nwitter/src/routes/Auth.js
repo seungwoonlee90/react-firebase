718920a4b80a7bb798d7b8dd048c59855bd64237
ethanlee <seungwoonlee90@gmail.com>
2021-08-30 15:08:21 +0900

    fixed Login Form onChange Function

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -4,7 +4,14 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const onChange = (event) => {
-        console.log(event.target.name)
+        const {
+            target: {name, value},
+        } = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
     }
     const onSubmit = (event) => {
         event.preventDefault();
