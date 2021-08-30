df44652ca3eb21d37beeeb787915914a42314f52
ethanlee <seungwoonlee90@gmail.com>
2021-08-30 15:58:46 +0900

    firebase authService

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { authService } from 'firebase';
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+
     const onChange = (event) => {
         const {
             target: {name, value},
@@ -13,8 +16,23 @@ const Auth = () => {
             setPassword(value);
         }
     }
-    const onSubmit = (event) => {
+    const onSubmit = async(event) => {
         event.preventDefault();
+        try {
+            let data;
+            if(newAccount) {
+                data = await authService.createUserWithEmailAndPassword(
+                    email, password
+                );
+            } else {
+                data = await authService.signInWithEmailAndPassword(
+                    email, password
+                );
+            }
+            console.log(data);
+        } catch(error) {
+            console.log(error);
+        }
     }
 
     return (
@@ -22,7 +40,7 @@ const Auth = () => {
             <form onSubmit={onSubmit}>
                 <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-                <input type="submit" value="LogIn"/>
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"}/>
             </form>
             <div>
                 <button>Continue with google</button>
