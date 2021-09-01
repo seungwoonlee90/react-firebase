c21a741576eac69548260ba42cca51dec4d24a4a
ethanlee <seungwoonlee90@gmail.com>
2021-09-01 09:38:54 +0900

    set firebase Log In

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppRouter from './Router';
 import { authService } from '../firebase';
 
 function App() {
-  console.log(authService.currentUser);
-  const [isLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  });
   return(
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   )
