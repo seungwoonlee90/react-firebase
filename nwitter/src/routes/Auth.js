ce2e44529003b5ce37e18399339998da37e77748
ethanlee <seungwoonlee90@gmail.com>
2021-09-02 16:49:10 +0900

    Auth.js account error

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -7,7 +7,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
-
+    const [error, setError] = useState("");
     const onChange = (event) => {
         const {
             target: {name, value},
@@ -33,9 +33,11 @@ const Auth = () => {
             }
             console.log(data);
         } catch(error) {
-            console.log(error);
+            setError(error);
         }
     }
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
 
     return (
         <div>
@@ -43,7 +45,9 @@ const Auth = () => {
                 <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
                 <input type="submit" value={newAccount ? "Create Account" : "Log In"}/>
+                {error}
             </form>
+    <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Cerate Account"}</span>
             <div>
                 <button>Continue with google</button>
             </div>
